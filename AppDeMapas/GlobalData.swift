4f1ceb5fdb83af7eec6c2e7391efe083f4d2d219
PicPoint <apppicpoint@gmail.com>
2019-02-11 11:51:42 +0100

    jklñ

---
+++ AppDeMapas/GlobalData.swift
@@ -0,0 +1,21 @@
+//
+//  GlobalData.swift
+//  AppDeMapas
+//
+//  Created by alumnos on 28/1/19.
+//  Copyright © 2019 alumnos. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+//Donde almaceno los datos de las ubicaciones
+var titulos = [String]()
+var comentarios = [String]()
+var fechaDesde = [String]()
+var fechaHasta = [String]()
+//Coordenadas de las ubicaciones creadas
+var coordenadasA:[Double] = [2.1]
+var coordenadasB:[Double] = [1.2]
+//Son arrays en los que meto la info que me devuelve la base de datos
+var Spots:[Spot] = []
